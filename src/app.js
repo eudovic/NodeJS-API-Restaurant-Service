38e1b1c444f52eb679781406c55e3a35bf458f5f
ivanandra <ivanandra@gmail.com>
2021-12-07 18:31:31 -0300

    Gravaçao Concluida no BD

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 var express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 var cors = require('cors');
 var routes = require('./routes/ApiRoutes');
 const app = express();
 app.use(cors());
 const port = 3337
 
- app.use(express.static(path.join(__dirname, '../modogarcom/out/')));
-
+app.use(express.static(path.join(__dirname, '../modogarcom/out/')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.json());
 app.use(routes);
 
--- src/controllers/ProdutosController.js
+++ src/controllers/ProdutosController.js
@@ -1,5 +1,6 @@
 const connection = require('../database/connection');
 const dbSettings = require("../../dbSettings");
+const { json } = require('body-parser');
 
 module.exports = {
     async index(req, res) {
@@ -15,57 +16,55 @@ module.exports = {
             data: produtos
         });
     },
-     async sendProduct(req, res) {
-        const products = req.body;       
-        console.log(req.method);
-        if (req.method === "POST") {
-        //  const saveExpense =  await connection(dbSettings.expenses_table)
-        //   .insert(dbSettings.expenses_expenses_id_product_col = products.id_produto_ou_passeio,
-        //     dbSettings.expenses_expenses_name_expense_col = products.nome_produto,
-        //     dbSettings.expenses_expenses_amount_col = products.valor_gasto,
-        //     dbSettings.expenses_expenses_quantity_col = products.qtde_produto,
-        //     dbSettings.expenses_expenses_number_order_pad_col = 80)
-          return res.status(200).json({
-              
-              data: products
-            });
+       async sendProduct(req, res) {
+        const products = req.body.arrayLimpaGasto;
+        var cleanObservation = {};
+        products.map((product) => {
             
-        } else {
-          return res.status(200).json({
-              message: "deu errado ",
+            cleanObservation = product.observacao;
+        })
+
+        var dadosGasto = [];
+        var dadosObservacoes = [];
+        products.forEach((product) => {
+           
+            const expensesData = {
+                [dbSettings.expenses_expenses_name_expense_col]: product.nome_produto,
+                [dbSettings.expenses_expenses_id_product_col]: product.id_produto_ou_passeio, 
+                [dbSettings.expenses_expenses_quantity_col]: product.qte_produto,
+                [dbSettings.expenses_expenses_type_expense_col]: 'produtos',
+                [dbSettings.expenses_expenses_amount_col]: product.valor_gasto,
+                [dbSettings.expenses_expenses_number_order_pad_col]: product.comanda[0].numero_comanda_status,
+                [dbSettings.expenses_expenses_id_user_expense]: product.comanda[0].id_cliente_gasto 
+            }
+            dadosGasto.push(expensesData);
+        });
+
+        if (cleanObservation != null) {
+            let obsData = cleanObservation.filter((value, index) => {
+                return value != ' ';
+            })
+
+            obsData.forEach((obs) => {
+
+                const observationData = {
+                    [dbSettings.observations_observation_col]: obs.nome_obs,
+                    [dbSettings.observations_product_id]: obs.produto_id
+                }
+                dadosObservacoes.push(observationData);
+            })
+
+            await connection(dbSettings.observations_table)
+            .insert(dadosObservacoes).into(dbSettings.observations_table)
+            
+        }     
+        await connection(dbSettings.expenses_table)
+            .insert(dadosGasto).into(dbSettings.expenses_table)
+            .then(function(data) {
+            
+                res.send(data);
             });
-          }
+       
       }
 
-
-        // const gravaProdutos = await connection(dbSettings.expenses_table)
-        // .insert(
-        //     dbSettings.expenses_expenses_id_product_col,
-        //     dbSettings.expenses_expenses_name_expense_col,
-        //     dbSettings.expenses_expenses_amount_col
-        // );
-        // return res.json({
-        //         success: true,
-        //         data: gravaProdutos
-        //         })
-    
-         
-
-
-       // const gravaProdutos = await connection(dbSettings.expenses_table)
-         //.insert(
-    //         dbSettings.expenses_expenses_id_col,
-           //  dbSettings.expenses_expenses_name_expense_col,
-           //  dbSettings.expenses_expenses_price_col,
-    //         // dbSettings.expenses_expenses_number_order_pad_col,
-    //         // dbSettings.expenses_expenses_type_expense_col,
-    //         // dbSettings.expenses_expenses_id_product_col,
-    //         // dbSettings.expenses_expenses_quantity_col,
-    //         dbSettings.expenses_expenses_amount_col
-    //     );
-    //     return res.json({
-    //         success: true,
-    //         data: gravaProdutos
-    //     })
-    // }
 }
